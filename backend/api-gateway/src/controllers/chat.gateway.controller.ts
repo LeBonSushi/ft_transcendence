d1839d2868eb649c876dd72c8999042d4fb1f1c8
Maxence Corso <maxence.corso2204@laposte.net>
2025-12-09 12:11:41 +0100

    added / route for all the apis

--- backend/api-gateway/src/controllers/chat.gateway.controller.ts
+++ backend/api-gateway/src/controllers/chat.gateway.controller.ts
@@ -5,6 +5,13 @@ import { ProxyService } from '../services/proxy.service';
 export class ChatGatewayController {
 	constructor(private proxy: ProxyService) {}
 
+	@All()
+	async handleAuthRoot(@Req() req, @Res() res) {
+		const targetUrl = `http://service-chat:4002/chat`;
+		const result = await this.proxy.forwardRequest(req.method, targetUrl, req.body, req.headers);
+		res.send(result);
+	}
+
 	@All('*')
 	async handleChatRequests(@Req() req, @Res() res) {
 		const path = req.url === '/' ? '' : req.url;
--- backend/api-gateway/src/controllers/game.gateway.controller.ts
+++ backend/api-gateway/src/controllers/game.gateway.controller.ts
@@ -5,6 +5,13 @@ import { ProxyService } from '../services/proxy.service';
 export class GameGatewayController {
 	constructor(private proxy: ProxyService) {}
 
+	@All()
+	async handleGameRoot(@Req() req, @Res() res) {
+		const targetUrl = `http://service-game:4003/game`;
+		const result = await this.proxy.forwardRequest(req.method, targetUrl, req.body, req.headers);
+		res.send(result);
+	}
+
 	@All('*')
 	async handleGameRequests(@Req() req, @Res() res) {
 		const path = req.url === '/' ? '' : req.url;
