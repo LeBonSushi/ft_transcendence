b1d47917c6ad626e38af1f362d543167e4fe70b6
Maxence Corso <57074437+LeBonSushi@users.noreply.github.com>
2026-02-13 17:01:43 +0100

    Finished search bar for users to add friend

--- apps/web/src/app/test-search/page.tsx
+++ apps/web/src/app/test-search/page.tsx
@@ -1,9 +1,10 @@
 import { FriendSearchBar } from "@/components/FriendSearchBar";
+import FriendSearchBarServer from "@/components/server/FriendSearchBarServer";
 
 export default function Page() {
   return (
     <div className="flex justify-center h-screen items-center">
-      <FriendSearchBar/>
+      <FriendSearchBarServer/>
     </div>
   )
 }
--- apps/web/src/components/FriendSearchBar.tsx
+++ apps/web/src/components/FriendSearchBar.tsx
@@ -4,13 +4,17 @@ import { apiClient } from "@/lib/api";
 import { API_ROUTES, SearchUser } from "@travel-planner/shared";
 import { useEffect, useState } from "react";
 import { Avatar } from "./ui";
+import { set } from "date-fns";
 
 export function FriendSearchBar() {
   const [search, setSearch] = useState("");
   const [users, setUsers] = useState<SearchUser[]>([]);
 
   useEffect(() => {
-    if (search.length <= 0) return;
+    if (search.length <= 0) {
+      setUsers([]);
+      return;
+    }
 
     apiClient.get<SearchUser[]>(API_ROUTES.USERS.SEARCH(search)).then((res) => {
       setUsers(res);
@@ -27,7 +31,7 @@ export function FriendSearchBar() {
         placeholder="Username"
         className="w-full bg-transparent outline-none placeholder:text-muted-foreground"
       />
-      {users.length > 0 && (
+      {users.length > 0 && search.length > 0 && (
         <div className="absolute left-0 top-full mt-2 w-full bg-popover border-2 border-border rounded-lg shadow-lg z-10">
           {users.map((user) => (
             <div key={user.id} className="flex items-center px-4 py-2 hover:bg-secondary cursor-pointer gap-3">
